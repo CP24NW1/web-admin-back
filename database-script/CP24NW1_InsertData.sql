5871fd76f4d6bb9a3b54eb4a9751da326099b2ed
Supakorn Chat-anothai <102203151+64130500111@users.noreply.github.com>
2025-03-20 15:55:12 +0700

    Update CP24NW1_InsertData.sql

--- database-script/CP24NW1_InsertData.sql
+++ database-script/CP24NW1_InsertData.sql
@@ -29,5 +29,22 @@ INSERT INTO permission (permission_id, permission) VALUES
 
 
 -- INSERT ADMIN 
-INSERT INTO user(firstname, lastname, email, DOB, password, is_verify) values("admin", "cp24nw1", "nw1admin@mailinator.com", "2002-11-28", "$2y$10$G6SlliDc2iqKBo7cb5NUj.dyS4meSKdxgi378vwhNRt3DyIYsWz/C", 1)
+INSERT INTO user(firstname, lastname, email, DOB, password, is_verify) values("admin", "cp24nw1", "nw1admin@mailinator.com", "2002-11-28", "$2b$10$FLZFnRG6EOnJmT7TA/cNyuVDRZD.wqNODac4HrX1AIa9fA/e4YJm.", 1);
+INSERT INTO user_permission(user_id, permission_id) values(1,1);
+INSERT INTO user_permission(user_id, permission_id) values(1,2);
+INSERT INTO user_permission(user_id, permission_id) values(1,3);
+INSERT INTO user_permission(user_id, permission_id) values(1,4);
+INSERT INTO user_permission(user_id, permission_id) values(1,5);
+INSERT INTO user_permission(user_id, permission_id) values(1,6);
+INSERT INTO user_permission(user_id, permission_id) values(1,7);
+INSERT INTO user_permission(user_id, permission_id) values(1,8);
+INSERT INTO user_permission(user_id, permission_id) values(1,9);
+INSERT INTO user_permission(user_id, permission_id) values(1,10);
+INSERT INTO user_permission(user_id, permission_id) values(1,11);
+INSERT INTO user_permission(user_id, permission_id) values(1,12);
+INSERT INTO user_permission(user_id, permission_id) values(1,13);
+INSERT INTO user_permission(user_id, permission_id) values(1,14);
+INSERT INTO user_permission(user_id, permission_id) values(1,15);
+INSERT INTO user_permission(user_id, permission_id) values(1,16);
+INSERT INTO user_permission(user_id, permission_id) values(1,17);
 
